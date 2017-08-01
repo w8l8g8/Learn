5ee05b34daab45238c6b98683b2ee4fe57f0626d
yuh_lei <yuh_lei@163.com>
2017-08-01 16:50:04 +0800

    e

---
+++ XinSha/src/main/java/com/xajiusuo/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.xajiusuo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	String message = "Hello, Welcome to Spring MVC!";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.println("in controller");
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+
+}
